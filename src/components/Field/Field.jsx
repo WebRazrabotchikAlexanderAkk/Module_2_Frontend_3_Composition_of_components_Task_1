7c33e8ad4d261e9fc7096ac8a3632d566c603299
Alexander Al <web-alexandr-akk@mail.ru>
2026-04-12 15:17:42 +0300

    Merge dev branch: Step 6-7 implementation
    - Add restartGame functionality
    - Add PropTypes to all Layout components
    - Add visual improvements: disabled states, colors for X/O, hover effects
    - Extract game logic to src/utils/gameLogic.js
    - Clean up temporary files

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import FieldLayout from './FieldLayout';
+import PropTypes from 'prop-types';
 
-const Field = () => {
-  return (
-    <div className="field">
-      <FieldLayout />
-    </div>
-  );
+const Field = ({ field, onCellClick, isGameEnded }) => {
+	return (
+		<div className="field">
+			<FieldLayout field={field} onCellClick={onCellClick} isGameEnded={isGameEnded} />
+		</div>
+	);
+};
+
+Field.propTypes = {
+	field: PropTypes.arrayOf(PropTypes.string).isRequired,
+	onCellClick: PropTypes.func.isRequired,
+	isGameEnded: PropTypes.bool,
 };
 
 export default Field;
--- src/components/Information/InformationLayout.jsx
+++ src/components/Information/InformationLayout.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './information.module.css';
+import PropTypes from 'prop-types';
 
-const InformationLayout = () => {
-  return (
-    <div className={styles.informationLayout}>
-      <div className={styles.informationText}>Ходит: X</div>
-    </div>
-  );
+const InformationLayout = ({ currentPlayer, isGameEnded, isDraw }) => {
+	if (isDraw) return <p className={styles.informationText}>Ничья</p>;
+	if (isGameEnded)
+		return <p className={styles.informationText}>Победа: {currentPlayer}</p>;
+	return <p className={styles.informationText}>Ходит: {currentPlayer}</p>;
+};
+
+InformationLayout.propTypes = {
+	currentPlayer: PropTypes.string.isRequired,
+	isGameEnded: PropTypes.bool.isRequired,
+	isDraw: PropTypes.bool.isRequired,
 };
 
 export default InformationLayout;
