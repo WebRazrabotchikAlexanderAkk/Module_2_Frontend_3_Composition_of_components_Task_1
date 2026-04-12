ca62fee1352f59016d9c2d7626788081d6dd3f63
Alexander Al <web-alexandr-akk@mail.ru>
2026-04-12 15:08:29 +0300

    fix information

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import InformationLayout from './InformationLayout';
+import PropTypes from 'prop-types';
 
 const Information = ({ currentPlayer, isGameEnded, isDraw }) => {
 	return (
@@ -11,6 +12,12 @@ const Information = ({ currentPlayer, isGameEnded, isDraw }) => {
 			/>
 		</div>
 	);
+};
+
+Information.propTypes = {
+	currentPlayer: PropTypes.string.isRequired,
+	isGameEnded: PropTypes.bool.isRequired,
+	isDraw: PropTypes.bool.isRequired,
 };
 
 export default Information;
